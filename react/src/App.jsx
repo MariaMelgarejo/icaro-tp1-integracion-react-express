5336c43cd9438acd64eadba5370ddaf63eb5cb96
Maria Elena Melgarejo <melgarejo_maria@hotmail.com>
2023-08-22 15:55:33 -0300

    Peticion de productos al servidor

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [productos, setProductos] = useState([])
+
+  useEffect(() => {
+    const url = 'http://localhost:8080/productos'
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setProductos(data)
+      })
+  }, [])
 
   return (
     <>
