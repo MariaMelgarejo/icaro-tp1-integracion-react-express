7bdfe3e699bfaeb75171fdb161c3408d0b931928
Maria Elena Melgarejo <melgarejo_maria@hotmail.com>
2023-08-22 13:48:34 -0300

    Creación de ruta de productos. Servidor terminado

--- express/src/app.js
+++ express/src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const PORT = 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/productos', (req, res) => {
+    axios.get('https://fakestoreapi.com/products')
+        .then(resp => resp.data)
+        .then(data => {
+            res.header("Access-Control-Allow-Origin", "*")
+            res.send(data);
+        });
 });
